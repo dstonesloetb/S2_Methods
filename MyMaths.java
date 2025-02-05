6d7728db6542caa7de74ca6f81ef889a75c2f99d
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-02-05 14:57:32 +0000

    Create MyMaths.java

---
+++ MyMaths.java
@@ -0,0 +1,26 @@
+public class MyMaths
+{
+
+  public static double volumeOfCube(double side)
+  {
+    //double volume = side * side * side; 
+    //return volume; 
+    
+    //return Math.pow(side,3);
+    
+    return side * side * side;
+    
+  }  //end volumeOfCube
+
+
+  public static double areaTriangle(double height, double base)
+  {
+   // double area = 0.5 * base * height;
+   // return area;
+    
+    return 0.5d* base * height;
+    
+  } //end areaTriangle 
+
+  
+} //end class
